b900a74544841f18b2045454e9c9fa8c47a7ddf8
aswistak@linkpoint.ca <swistek.labs@gmail.com>
2020-10-27 18:43:08 -0700

    Debugged using 'truffle debug' found error, corrected, all tests pass.

--- project-dir/migrations/2_deploy_contract.js
+++ project-dir/migrations/2_deploy_contract.js
@@ -1,5 +1,7 @@
 var SimpleStorage = artifacts.require('SimpleStorage')
+var Migrations = artifacts.require('Migrations')
 
 module.exports = (deployer) => {
     deployer.deploy(SimpleStorage)
+    deployer.deploy(Migrations)
 }
